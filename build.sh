765643eeae397674e5f62301c8a35e40c93bd80f
MattKenyei <99202291+MattKenyei@users.noreply.github.com>
2025-06-22 22:58:26 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --version 6.0.423
+# Установка .NET 9.0
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --version 9.0.100-preview.7
 
+# Добавляем .NET в PATH
 export PATH="$HOME/.dotnet:$PATH"
 
+# Сборка проекта
 cd ReactApp1.Server
 dotnet publish -c Release -o ./publish
