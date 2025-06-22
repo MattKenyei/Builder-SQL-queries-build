85cb06d1382d92f5f8c5dccb9a271d3402960dbd
MattKenyei <99202291+MattKenyei@users.noreply.github.com>
2025-06-22 23:36:48 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 set -e  # Прерывать при ошибках
 
-# Установка .NET 8.0
-curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin \
-    --version 8.0.204 \
-    --install-dir /opt/dotnet
-
-export DOTNET_ROOT=/opt/dotnet
+# Установка .NET в домашнюю директорию
+export DOTNET_ROOT=$HOME/.dotnet
 export PATH="$DOTNET_ROOT:$PATH"
+
+# Установка .NET 8.0 (если еще не установлен)
+if ! command -v dotnet &> /dev/null; then
+    curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin \
+        --version 8.0.204 \
+        --install-dir $HOME/.dotnet
+fi
+
+# Проверка установки
+dotnet --version
 
 # Сборка бекенда
 cd ReactApp1.Server
@@ -17,7 +23,6 @@ dotnet publish -c Release -o ./publish
 # Сборка фронтенда
 cd ../reactapp1.client
 
-# Исправление прав доступа для Vite
-npm install -g vite
+# Установка зависимостей и сборка
 npm install --unsafe-perm
 npm run build
